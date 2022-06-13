8124b284d08d59a1758f9a6623077c2f00d76205
triplek(vm) <triplek@triplek.net>
2022-06-13 17:28:16 +0900

    7.0 To Do List part One

--- src/App.js
+++ src/App.js
@@ -1,37 +1,30 @@
-import Button from "./Button";
-import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
-/**
- * Cleaning function 사용 예제
- * Cleaning function이란 컴포넌트가 destroy 될 때 실행되는 function 이다.
- */
-
-function Hello() {
-  /* arrow function 사용 예제 */
-  useEffect(() => {
-    console.log("hi :)");
-    return () => console.log("bye :(");
-  }, []);
-
-  /* 기본 function 사용 예제 */
-  useEffect(function () {
-    console.log("hi :)");
-    return function () {
-      console.log("bye :(");
-    };
-  }, []);
-  return <h1>Hello!!</h1>;
-}
-
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    setToDos((prevArray) => [toDo, ...prevArray]);
+    setToDo("");
+  };
 
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Do Lists ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          value={toDo}
+          onChange={onChange}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add to do</button>
+      </form>
     </div>
   );
 }
