51b23d54c6a11f4fba2990f6629b2758cad1b1dc
triplek(vm) <triplek@triplek.net>
2022-06-13 14:07:56 +0900

    6.0 EFFECT Introduction

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import { useState } from "react";
 
 function App() {
+  const [value, setValue] = useState(0);
+  const onClick = () => setValue((prev) => prev + 1);
+  /**
+   * 다음시간에 배울 내용 useEffect()
+   * 기본적으로 state가 변경될 경우 App 내의 모든 컴포넌트가 re-rendering 된다.
+   * 그러나 렌더링될 때마다 실행되지 않아야 할 코드가 있을 경우(초기화 및 API 호출 등)
+   * 한 번만 코드가 살행되도록 (또는 조건을 줄 수 있음) 하는 부분은 위한 코드 예제
+   * */
+  console.log("rendered");
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!</h1>
-      <Button text="Continue" />
+      <h1 className={styles.title}>{value}</h1>
+      <Button text="Continue" onClick={onClick} />
     </div>
   );
 }
