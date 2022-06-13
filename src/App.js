cb9cfec8cd9dac35a9e26bbec2963468d57d34b7
triplek <triplek@triplek.net>
2022-06-14 00:04:28 +0900

    7.2 Coins

--- src/App.js
+++ src/App.js
@@ -1,36 +1,31 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] = useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (toDo === "") {
-      return;
-    }
-    setToDos((prevArray) => [toDo, ...prevArray]);
-    setToDo("");
-  };
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
 
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
   return (
     <div>
-      <h1>My To Do Lists ({toDos.length})</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          value={toDo}
-          onChange={onChange}
-          type="text"
-          placeholder="Write your to do..."
-        />
-        <button>Add to do</button>
-      </form>
-      <hr />
-      <ul>
-        {toDos.map((item, idx) => (
-          <li key={idx}>{item}</li>
-        ))}
-      </ul>
+      <h1>The Coins! ({coins.length})</h1>
+      {loading ? (
+        "Loading..."
+      ) : (
+        <select>
+          {coins.map((coin, idx) => (
+            <option key={idx} value={coin.id}>
+              {coin.name} ({coin.symbol}): {coin.quotes.USD.price} USD}
+            </option>
+          ))}
+        </select>
+      )}
     </div>
   );
 }
